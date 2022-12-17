74e9797a6cc3d3805cad139ae92528c8f43c469a
OmarGomaaFCi <omargomaafcitu@gmail.com>
2022-12-17 07:06:55 +0200

    Modal Bottom Sheet Commit

--- app/src/main/java/com/example/bottomsheetdemo/MainActivity.kt
+++ app/src/main/java/com/example/bottomsheetdemo/MainActivity.kt
@@ -1,17 +1,12 @@
 package com.example.bottomsheetdemo
 
-import android.app.Dialog
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Gravity
-import android.view.ViewGroup
-import android.view.Window
+import android.view.LayoutInflater
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.Toast
-import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 /*
 TODO:
@@ -19,71 +14,49 @@ TODO:
 * 2- Bottom Sheet Layout
 * 3- Programmatically
  */
+
 class MainActivity : AppCompatActivity() {
+    private lateinit var modalBottomSheetDialog: BottomSheetDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btn = findViewById<Button>(R.id.btnShowButtomSheet)
-
-        btn.setOnClickListener {
-            BottomSheetBehavior.from(findViewById(R.id.bottomSheetStandard)).apply {
-                this.state = BottomSheetBehavior.STATE_COLLAPSED
-            }
-//            showBottomSheet()
+        val btnModalSheet = findViewById<Button>(R.id.btnShowModalBottomSheet)
+        btnModalSheet.setOnClickListener {
+            showModalBottomSheet()
         }
 
-        BottomSheetBehavior.from(findViewById(R.id.bottomSheetStandard)).apply {
-            this.state = BottomSheetBehavior.STATE_HIDDEN
-            /*
-       * STATE_COLLAPSED: The bottom sheet is visible but only showing its peek height. This state is usually the ‘resting position’ of a Bottom Sheet.
-       * STATE_EXPANDED: The bottom sheet is visible and its maximum height and it is neither dragging nor settling
-       * STATE_DRAGGING: The user is actively dragging the bottom sheet up or down.
-       * STATE_HIDDEN: The bottom sheet is no longer visible.
-             */
-        }
-    }
 
-    private fun showBottomSheet() {
-        // Setup the bottom sheet as a dialogue
-        val dialog = Dialog(this)
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
-        dialog.setContentView(R.layout.bottom_sheet_layout)
-
-
-        val createLayout = dialog.findViewById<LinearLayout>(R.id.createLayout)
-        val deleteLayout = dialog.findViewById<LinearLayout>(R.id.deleteLayout)
-        val exitLayout = dialog.findViewById<LinearLayout>(R.id.exitLayout)
-
-        createLayout.setOnClickListener {
-            onItemDialogListener(dialog, "Created")
-        }
-
-        deleteLayout.setOnClickListener {
-            onItemDialogListener(dialog, "Deleted")
-        }
-
-        exitLayout.setOnClickListener {
-            onItemDialogListener(dialog, "Exit!!")
-        }
-
-        dialog.apply {
-            show()
-            window?.apply {
-                setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
-                setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-                attributes.windowAnimations = R.style.BottomSheetTheme
-                setGravity(Gravity.BOTTOM)
-
-            }
-        }
-
+        /*
+* STATE_COLLAPSED: The bottom sheet is visible but only showing its peek height. This state is usually the ‘resting position’ of a Bottom Sheet.
+* STATE_EXPANDED: The bottom sheet is visible and its maximum height and it is neither dragging nor settling
+* STATE_DRAGGING: The user is actively dragging the bottom sheet up or down.
+* STATE_HIDDEN: The bottom sheet is no longer visible.
+  */
 
     }
 
-    private fun onItemDialogListener(dialog: Dialog, mess: String) {
-        dialog.dismiss()
-        Toast.makeText(this, mess, Toast.LENGTH_SHORT).show()
+    private fun showModalBottomSheet() {
+        modalBottomSheetDialog = BottomSheetDialog(this)
+
+        // دي ماسكة  ال bottom sheet
+        val sheetView = LayoutInflater.from(applicationContext)
+            .inflate(R.layout.bottom_sheet_layout, findViewById(R.id.bottomSheetLayout), false)
+
+        sheetView.findViewById<LinearLayout>(R.id.createLayout).setOnClickListener {
+            Toast.makeText(this, "Create", Toast.LENGTH_SHORT).show()
+        }
+        sheetView.findViewById<LinearLayout>(R.id.deleteLayout).setOnClickListener {
+            Toast.makeText(this, "Delete", Toast.LENGTH_SHORT).show()
+        }
+        sheetView.findViewById<LinearLayout>(R.id.exitLayout).setOnClickListener {
+            Toast.makeText(this, "Exit", Toast.LENGTH_SHORT).show()
+        }
+
+        // Link sheet view with bottomSheetDialog
+        modalBottomSheetDialog.setContentView(sheetView)
+        modalBottomSheetDialog.show()
     }
 
 
